f38de83e8636e8b78cb3cf32cd33791c9bb696aa
Max Buras <max.thomae@gmail.com>
2019-12-11 19:46:27 +0100

    Added missing animations and book closing sound

--- js/book.js
+++ js/book.js
@@ -2,7 +2,8 @@ var isPlaying = false;
 var player = videojs('book-player');
 
 var pathPrefix = './assets/video/';
-var pageTurnSound = new Audio('./assets/audio/page-flip-03.mp3');
+var soundPageTurning = new Audio('./assets/audio/page-flip-03.mp3');
+var soundBookClosing = new Audio('./assets/audio/page-flip-9_heavy_book_closing.mp3');
 
 var currentVideoIndex = 0;
 
@@ -12,19 +13,29 @@ var KEY_PREVIOUS = 37; // arrow left
 var KEY_NEXT = 39; // arrow right
 
 var videos = [
+	'00-01-02.mp4',
 	'01-02-03-04.mp4',
 	'03-04-05-06.mp4',
-	'05-06-07-08.mp4'
+	'05-06-07-08.mp4',
+	'07-08-09-10.mp4',
+	'09-10-11-12.mp4',
+	'11-12-13-14.mp4',
+	'13-14-15-16.mp4',
+	'15-16-17-18.mp4',
+	'17-18-19-20.mp4',
+	'19-20-End.mp4'
 ];
 
 function togglePlayPause() {
-	if (isPlaying) {
-		player.pause();
-		isPlaying = false;
-	} else {
+	if (player.paused()) {
 		player.play();
-		isPlaying = true;
-		pageTurnSound.play();
+		if (currentVideoIndex == videos.length - 1) {
+			soundBookClosing.play();
+		} else {
+			soundPageTurning.play();
+		}
+	} else {
+		player.pause();
 	}
 }
 
@@ -71,10 +82,10 @@ player.ready(function() {
 	}
 
 	player.on("ended", function() {
-		isPlaying = false;
 		if (currentVideoIndex < videos.length - 1) {
 			currentVideoIndex++;
 			player.src(generateSourcesFromFile(videos[currentVideoIndex]));
 		}
+		
 	})
 })
